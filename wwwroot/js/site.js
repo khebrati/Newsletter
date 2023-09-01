e165db749e6c96bfefe1a2e75895fa9c06b42b07
ErfanKhebrati <khebrati@gmail.com>
2023-09-01 21:01:51 +0330

    fixed post page layout with flex box

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,4 @@
-﻿window.setTimeout(function(){
-    window.location.href='https://localhost:7220/';
-    }, 4000);
+﻿//TODO: make this happen in production
+// window.setTimeout(function(){
+//     window.location.href='https://localhost:7220/';
+//     }, 4000);
